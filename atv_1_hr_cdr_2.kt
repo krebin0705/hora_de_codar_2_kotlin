9e412154d07aa2c3cc7aa31b9554dcb4a54d31da
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-03-11 12:58:59 -0300

    Add files via upload
    atv 1 hr de codar 2 feita

---
+++ atv_1_hr_cdr_2.kt
@@ -0,0 +1,43 @@
+fun main() {
+
+//Escreva um programa em que o usuário informe dois números. Então escreva em tela o maior deles.
+
+
+
+
+    println("informe o primeiro número: ")
+   var a = readln()
+
+
+    println("informe o segundo número: ")
+    var b = readln()
+
+
+    if( a > b) {
+        println("$a é o maior número!")
+    }
+
+    else{
+        println("$b é o maior número!")
+    }
+
+   if( a == b){
+       println("Os números são iguais")
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
