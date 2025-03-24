22516e539b03a817a9248562c08218902412c39b
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-03-24 12:48:21 -0300

    Update atv_5.kt

--- atv_5.kt
+++ atv_5.kt
@@ -1,1 +1,14 @@
+//Faça um programa que leia 3 valores informados pelo usuário
+// (considere que não serão informados valores iguais) e escrever a soma dos 2 maiores.
+
+  fun main() {
+    println("Digite 6 números:")
+    val numeros = List(6) {
+      print("Número ${it + 1}: ")
+      //it é de 0 a 5, por isso o "+1" pra ficar de 0 a 6
+      readLine()?.toDoubleOrNull() ?: return println("Entrada inválida. Tente novamente.")
+    }
+    println("Números informados: $numeros")
+    println("Média aritmética: ${numeros.average()}")
+  }
 
