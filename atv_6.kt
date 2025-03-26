c71a1f4141e69242a3aec129621515b686fee002
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-03-26 10:30:23 -0300

    Update atv_6.kt

--- atv_6.kt
+++ atv_6.kt
@@ -1,1 +1,25 @@
+fun main(){
+//Faça um programa que receba quatro valores informados pelo usuário, mas informe somente o primeiro,
+// o último e o maior de todos eles (considere que todos os números informados serão diferentes)
+    val numeros = mutableListOf<Int>()
 
+    for (i in 1..4) {
+        print("Digite o ${i}º número: ")
+        val numero = readLine()?.toIntOrNull()
+        if (numero != null) {
+            numeros.add(numero)
+        } else {
+            println("Entrada inválida. Tente novamente.")
+            return
+        }
+    }
+
+    val primeiro = numeros.first()
+    val ultimo = numeros.last()
+    val maior = numeros.maxOrNull()
+
+    println("Primeiro número: $primeiro")
+    println("Último número: $ultimo")
+    println("Maior número: $maior")
+
+}
