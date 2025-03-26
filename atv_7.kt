4a3650c3a482fc0885419ddbc9c4efb522c7b8e5
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-03-26 11:03:02 -0300

    Update atv_7.kt

--- atv_7.kt
+++ atv_7.kt
@@ -1,1 +1,27 @@
+//Faça um programa que leia 6 números que o usuário vai informar.
+// Todos os números lidos com valor inferior a 72 devem ser somados.
+// Escreva o valor final da soma efetuada e também todos valores que o usuário informou.
 
+fun main(){
+
+    val numeros = mutableListOf<Int>()
+    var soma = 0
+
+    for (i in 1..6) {
+        println("Digite o ${i}º número: ")
+        val numero = readLine()?.toIntOrNull()
+        if (numero != null) {
+            numeros.add(numero)
+            if( numero < 72) {
+                soma =+ numero
+            }
+        } else {
+            println("Entrada inválida. Tente novamente.")
+            return
+        }
+    }
+println("números informados: $numeros")
+println("soma dos números inferiores a 72 : $soma")
+
+
+}
