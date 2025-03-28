9957474cb916e0802ae7b81da4067cf55aee6faa
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-03-28 10:10:07 -0300

    atv_8.kt

--- atv_8.kt
+++ atv_8.kt
@@ -1,1 +1,33 @@
+fun main() {
+    val numeros = mutableListOf<Double>()
+    var contador = 1
 
+    println("Digite quatro números entre 0 e 10:")
+
+    while (numeros.size < 4) {
+        print("Número $contador: ")
+        val input = readLine()
+
+        try {
+            val numero = input?.toDouble() ?: throw NumberFormatException()
+
+            if (numero > 0 || numero < 10) {
+                numeros.add(numero)
+                contador++
+            } else {
+                println("Número inválido! Digite um valor entre 0 e 10.")
+            }
+        } catch (e: NumberFormatException) {
+            println("Entrada inválida! Digite um número válido.")
+        }
+    }
+
+    val media = numeros.average()
+    println("\nMédia calculada: $media")
+
+    if (media > 5) {
+        println("Você passou no teste!")
+    } else {
+        println("Tente novamente")
+    }
+}
