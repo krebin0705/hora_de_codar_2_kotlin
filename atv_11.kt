fddceeb2389e06c5b77d62ee9880049dc1ce09a1
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-03-28 10:20:06 -0300

    atv_11.kt

--- atv_11.kt
+++ atv_11.kt
@@ -1,1 +1,25 @@
+fun main() {
+    println("Calculadora Simples")
+    
+    print("Digite o primeiro número: ")
+    val num1 = readLine()!!.toInt()
+    
+    print("Digite o segundo número: ")
+    val num2 = readLine()!!.toInt()
+    
+    println("\nEscolha a operação:")
+    println("1 - Soma")
+    println("2 - Subtração")
+    println("3 - Divisão")
+    println("4 - Multiplicação")
+    print("Opção: ")
+    val op = readLine()!!.toInt()
 
+    when (op) {
+        1 -> println("Resultado: ${num1 + num2}")
+        2 -> println("Resultado: ${num1 - num2}")
+        3 -> println("Resultado: ${num1.toDouble() / num2}")
+        4 -> println("Resultado: ${num1 * num2}")
+        else -> println("Opção inválida!")
+    }
+}
